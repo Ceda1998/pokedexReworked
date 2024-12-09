d2fb88918e4f70bcf9f3cd36cda1deea49f91ee8
Cedric Daunke <daunke40@gmail.com>
2024-12-09 17:07:05 +0100

    basic card design and api information on card

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,9 @@
 //fetch Data:
+let pokeSprite;
+let pokeName;
+let pokeType;
+let pokeWeight;
+
 
 async function getData() {
     const url = "https://pokeapi.co/api/v2/pokemon/bulbasaur";
@@ -10,7 +15,41 @@ async function getData() {
   
       const pokeData = await response.json();
       console.log(pokeData);
+      pokeSprite = pokeData.sprites.front_shiny;
+      pokeName = pokeData.species.name;
+      pokeWeight = pokeData.weight;
+      pokeType = pokeData.types[0].type.name;
     } catch (error) {
       console.error(error.message);
     }
+    changeImg();
+    changeName();
+    changeWeight();
+    changeType();
   }
+
+  function changeImg() {
+    let picture = document.getElementById('pokeSprite');
+    picture.src = pokeSprite; 
+}
+
+  function changeDescription() {
+
+  }
+
+  function changeWeight() {
+    let pokeWeightText = document.getElementById('pokeWeight');
+    pokeWeightText.innerHTML = pokeWeight + 'kg';
+  }
+
+  function changeType() {
+    let pokeTypetText = document.getElementById('pokeType');
+    pokeTypetText.innerHTML = pokeType;
+  }
+
+  function changeName() {
+    let pokeNameText = document.getElementById('pokeName');
+    console.log(pokeNameText);
+    pokeNameText.innerHTML = pokeName;
+  }
+
